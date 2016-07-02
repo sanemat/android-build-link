1ef61e2b0fd7f856e8fdc33703396376a3ca045b
Sanemat <o.gata.ken@gmail.com>
2016-07-03 03:49:39 +0900

    use text from intent

--- app/src/main/java/jp/sane/buildlink/BuildLinkActivity.java
+++ app/src/main/java/jp/sane/buildlink/BuildLinkActivity.java
@@ -1,11 +1,14 @@
 package jp.sane.buildlink;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class BuildLinkActivity extends AppCompatActivity {
 
@@ -25,6 +28,15 @@ public class BuildLinkActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
 
 }
