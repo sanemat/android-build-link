6099bf0619de3100a82eda81ec13a8f222c18fb2
Sanemat <o.gata.ken@gmail.com>
2016-07-03 03:44:18 +0900

    add build link activity

--- app/src/main/java/jp/sane/buildlink/MainActivity.java
+++ app/src/main/java/jp/sane/buildlink/MainActivity.java
@@ -1,5 +1,6 @@
 package jp.sane.buildlink;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "jp.sane.buildlink.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +52,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buildLink(View view) {
+        Intent intent = new Intent(this, BuildLinkActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
